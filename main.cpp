80ce212de5313943754caae787a3d8302c26d688
Albertopostani <91824639+Albertopostani@users.noreply.github.com>
2021-12-01 21:51:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+bool numeriprimi(int n,int d){
+  int rest;
+  if(n>1){
+    if(d<n){
+      rest=n%d;
+      if(rest !=0){
+        return numeriprimi(n,d+1);
+      }else{
+        return false;
+}
+    }else{
+      return true;
+    }
+  }else if(n==2){
+    return true;
+  }else{
+    return false;
+  }
 }
