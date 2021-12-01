b7b9aaaa36e6ce63f911de9a8fe10565f8e07b7b
Albertopostani <91824639+Albertopostani@users.noreply.github.com>
2021-12-01 23:18:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,9 +3,9 @@ bool numeriprimi(int n,int d){
   int rest=0;
   if(n>1){
     if(d<n){
-      rest=n%d;
+      res t= n % d;
       if(rest !=0){
-        return numeriprimi(n,d+1);
+        return numeriprimi(n, d + 1);
       }else{
         return false;
 }
